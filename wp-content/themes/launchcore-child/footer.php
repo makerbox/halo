3c3b2e2c86557b3bc81b8afe046b666d2ad1357d
MattW <matt.werth@fivebyfiveglobal.com>
2021-08-12 15:06:28 +1000

    done ready for UAT

--- wp-content/themes/launchcore-child/footer.php
+++ wp-content/themes/launchcore-child/footer.php
@@ -1,23 +1,29 @@
-	<footer class="c-footer">
-		<div class="c-footer__inner">
-			<div class="c-footer__menu u-adaptive__mobile">
+	<footer class="p-footer">
+		<div class="p-footer__inner">
+			<div class="p-footer__left p-footer__half">
+				<div>
+					<div class="p-footer__headline">
+						company details
+					</div>
+					<div class="p-footer__details">
+						<div>P. <a href="tel:12341234">1234 1234</a></div>
+						<div>E. <a href="mailto:lorem@ipsum.com">lorem@ipsum.com</a></div>
+					</div>
+				</div>
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu') ); ?>
 			</div>
-			<div class="c-footer__bottom">
-				
-				<div class="c-footer__menu u-adaptive__desktop">
-					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu') ); ?>
-				</div>
-				<div class="o-copyright">
-					Copyright &copy; <?php echo esc_html( get_bloginfo( 'name' ) ); ?><?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?>
-				</div>
+			<div class="p-footer__right p-footer__half">
+				<?php 
+					$custom_logo_id = get_theme_mod( 'custom_logo' );
+					$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+				?>
+				<img src="<?php echo esc_url( $logo[0] ); ?>">
+			</div>
+			<div class="p-footer__copyright">
+				&copy; 	Copyright  <?php echo esc_html( get_bloginfo( 'name' ) ); ?><?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?>
 			</div>
 		</div>
-	</footer>
-	<?php wp_footer(); ?>
-</body>
-<script>
-	// log the template
-	console.log('Current template: <?php echo get_page_template(); ?>');
-</script>
+			</footer>
+			<?php wp_footer(); ?>
+		</body>
 </html>
