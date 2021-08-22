dd7ff732a14f5a6ad145b88007c60004c193baaa
MattW <matt.werth@fivebyfiveglobal.com>
2021-08-22 16:05:08 +1000

    contact form revival

---
+++ wp-content/themes/launchcore-child/src/js/components/contact-form.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	console.log('contact-form.js');
+	let inputFields = $(document).find('.c-contact-form__input-group input, .c-contact-form__input-group textarea');
+	// when you click a field remove the label
+	$(document).on('click touchend', '.c-contact-form__input-group', function(e){
+
+		let self = $(this);
+		self.find('.c-contact-form__label').addClass('is-hidden');
+	})
+	// when field changes
+	$('input, textarea').keyup(function(){
+		let self = $(this);
+		let myVal = self.val();
+		// if it is empty show the label
+		if(myVal.length){
+			self.parent().parent().find('.c-contact-form__label').addClass('is-hidden');
+		}else{
+			self.parent().parent().find('.c-contact-form__label').removeClass('is-hidden');
+		}
+	})
+}) 
--- wp-content/themes/launchcore-child/src/js/index.js
+++ wp-content/themes/launchcore-child/src/js/index.js
@@ -4,6 +4,7 @@
 import "./components/navbar.js";
 import "./components/next-button.js";
 import "./components/btc-feed.js";
+import "./components/contact-form.js";
 
 //templates
 
